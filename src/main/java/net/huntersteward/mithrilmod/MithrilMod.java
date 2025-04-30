37c8370ec6cde32de693cef3b62c191cfa5336bc
Clowdle <huntersteward26@gmail.com>
2025-04-30 01:04:38 -0500

    Initial commit

---
+++ src/main/java/net/huntersteward/mithrilmod/MithrilMod.java
@@ -0,0 +1,16 @@
+package net.huntersteward.mithrilmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MithrilMod implements ModInitializer {
+	public static final String MOD_ID = "mithrilmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/huntersteward/mithrilmod/mithrilModClient.java
@@ -0,0 +1,10 @@
+package net.huntersteward.mithrilmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mithrilModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // your code here
+    }
+}
