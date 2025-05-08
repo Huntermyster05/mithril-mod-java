f8d365bc3b69bcfe5bb61d108d2eb541c8c46198
Clowdle <huntersteward26@gmail.com>
2025-05-08 12:24:46 -0500

    Finished All Armor And Tools

---
+++ src/main/java/net/huntersteward/mithrilmod/util/ModTags.java
@@ -0,0 +1,27 @@
+package net.huntersteward.mithrilmod.util;
+
+import net.huntersteward.mithrilmod.MithrilMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_MITHRIL_TOOL = createTag("needs_mithril_tool");
+        public static final TagKey<Block> INCORRECT_FOR_MITHRIL_TOOL = createTag("incorrect_for_mithril_tool");
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(MithrilMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        public static final TagKey<Item> TRANSFORMABLE_ITEMS = createTag("transformable_items");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(MithrilMod.MOD_ID, name));
+        }
+    }
+}
